93c70587be37860f34d7d648b404509a2c70a516
“sher” <1433875305@qq.com>
2024-01-31 10:19:34 +0800

    “xxx”

---
+++ src/truck.js
@@ -0,0 +1,29 @@
+// 'use strict';
+
+// // 获取元素
+// var btn = document.querySelector('button');
+// // 绑定click事件，触发自定义事件
+// btn.addEventListener('click', function () {
+//     console.log("enter module");
+//     // Prevent the form from actually submitting
+//     event.preventDefault();
+//     var param1 = document.getElementById('param1').value;
+//     var param2 = document.getElementById('param2').value;
+//     var param3 = document.getElementById('param3').value;
+//     // 在控制台输出参数值，你可以根据需要进行其他操作
+//     console.log('参数1:', param1);
+//     console.log('参数2:', param2);
+//     console.log('参数3:', param3);
+//     // Print the form data
+//     // var form = document.getElementById("myForm");
+//     // for (var i = 0; i < form.elements.length; i++) {
+//     //     console.log(form.elements[i].name + ": " + form.elements[i].value);
+//     // }
+    
+// });
+
+// var collapseBtn = document.querySelector('.collapse-btn');
+// collapseBtn.addEventListener('click',function(){
+//     var inputForm = document.querySelector('.input-form');
+//     inputForm.classList.toggle('collapsed');
+// });
